fb2bedb2e61f172a9e0024321580d875af8979fe
MVP-Husan <chehsuan.chang@mvpfastlane.com>
2021-03-22 16:14:54 +0800

    update css, server config, pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from '../componments/Meta'
+
+const about = () => {
+  return (
+    <div>
+      <Meta title={'About'}/>
+      <h1>This is the about Page!</h1>
+    </div>
+  )
+}
+
+export default about;
