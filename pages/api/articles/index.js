374d1b26d6b420345e530750ffdb54e94ba27022
MVP-Husan <chehsuan.chang@mvpfastlane.com>
2021-03-24 13:49:31 +0800

    update article api

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -6,23 +6,15 @@ export const apiCaller = axios.create({
   headers: { 'Content-Type': 'application/json' },
 })
 
-export const getArticles = async () => {
-  const data = await apiCaller.get('/articles')
-    .then(res => res.data)
-    .catch(err => console.log(err));
-  return data
-}
-
-export const getArticleById = async (id) => {
-  const data = await apiCaller.get(`/articles/${id}`)
-    .then(res => res.data)
-    .catch(err => console.log(err));
-  return data
-}
-
 export default async function handler(req, res) {
   const data = await apiCaller.get('/articles')
     .then(res => res.data)
     .catch(err => console.log(err));
   return res.status(200).json(data);
 }
+
+export const getArticles = async () => await apiCaller.get('/articles').then(res => res.data);
+export const getArticleById = async (articleId) => await apiCaller.get(`/articles/${articleId}`).then(res => res.data);
+export const createArticle = async (article) => await apiCaller.post('/articles', article).then(res => res.data);
+export const removeArticle = async (articleId) => await apiCaller.delete(`/articles/${articleId}`).then(res => res.data);
+export const updateArticle = async (articleId, article) => await apiCaller.put(`/articles/${articleId}`, article).then(res => res.data);
